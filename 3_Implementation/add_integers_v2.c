32baf10255fb908bda3dc11e0bb43b67c6c125c0
Jayesh Prabakaran <sjayeshprabu@gmail.com>
2022-03-28 11:28:35 +0530

    Till makefile

--- 3_Implementation/add_integers_v2.c
+++ 3_Implementation/add_integers_v2.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
+#include "unity.h"
 int add_integers(int num1, int num2)
 {
     int sum=num1+num2;
     return sum;
 }
 
-int main()
+void test_add_integers()
 {
+    #if 0
     if(0==add_integers(0, 0))
     printf("Add function works.\n");
     else
@@ -21,5 +23,15 @@ int main()
     printf("Add function works for negative numbers.\n");
     else
     printf("Add function does not work for negative numbers.\n");
+    #endif
+    TEST_ASSERT_EQUAL(0,add_integers(0, 0));
+    TEST_ASSERT_EQUAL(30,add_integers(10, 20));
+    TEST_ASSERT_EQUAL(-30,add_integers(-10, -20));
+}
+#if 0
+int main()
+{
+    test_add_integers();
     return 0;
 }
+#endif
