d5dfe3bccdd35d86d4b649224a0bb63765be4265
Jayesh Prabakaran <sjayeshprabu@gmail.com>
2022-03-29 12:45:51 +0530

    final change over

--- 3_Implementation/project_main.c
+++ 3_Implementation/project_main.c
@@ -70,19 +70,19 @@ void fd_interest(float p, float r, float t)
     float calc1=1+(r/400);
     float calc2=pow( calc1,(4* (t/12) ) );
     float final=calc2*p;
-    printf("\nThe Maturity Amount at the end of %.2f months is Rupees %.2f",t,final);
+    printf("\nThe Maturity Amount at the end of %.2f months is Rupees %.2f",t,round(final));
 }
 
 void rd_interest(float p, float r, float t)
 {
     float i=r/400;
-    float calc1=pow(i,t/3);
+    float calc1=(pow(1+i,t/3));
     float calc2=calc1-1;
     float numerator=p*calc2;
     float calc3=pow((1+i),-0.333333);
     float denominator=1-calc3;
     float final=numerator/denominator;
-    printf("\nThe Maturity Amount at the end of %.2f months is Rupees %.2f",t,final);
+    printf("\nThe Maturity Amount at the end of %.2f months is Rupees %.2f",t,round(final));
 }
 
 void sip_interest(float p, float r, float t)
@@ -92,5 +92,5 @@ void sip_interest(float p, float r, float t)
     float calc2=calc1-1;
     float calc3=calc2/i;
     float final=p*calc3*(1+i);
-    printf("\nThe Maturity Amount at the end of %.2f months is Rupees %.2f",t,final);
+    printf("\nThe Maturity Amount at the end of %.2f months is Rupees %.2f",t,round(final));
 }
