946faad72fa9c7e2d5f2ff1bf70bbf5aff6753c6
Jayesh Prabakaran <sjayeshprabu@gmail.com>
2022-03-29 22:42:59 +0530

    asdf

--- 3_Implementation/test_project_main.c
+++ 3_Implementation/test_project_main.c
@@ -1,6 +1,6 @@
 #include "unity.h"
 
-extern void manual_test_add_integers();
+extern void manual_interest_test();
 
 void setUp(void)
 {
@@ -10,30 +10,21 @@ void tearDown(void)
 {
 }
 
-void automated_test_add_integers()
+void automated_interest_test()
 {
-    TEST_ASSERT_EQUAL(0, add_integers(0, 0));
-    TEST_ASSERT_EQUAL(30, add_integers(10, 20));
-    TEST_ASSERT_EQUAL(-10, add_integers(-10, -20));
-}
-
-void automated_test_add_overflow()
-{
-    TEST_ASSERT_EQUAL(0, add_integers(INT_MAX, 10));
-}
-
-void automated_test_add_underflow()
-{
-    TEST_ASSERT_EQUAL(0, add_integers(INT_MIN, -1));
+    TEST_ASSERT_EQUAL(1086,fd_interest(1000,10,10));
+    TEST_ASSERT_EQUAL(12329,rd_interest(1000,5,12));
+    TEST_ASSERT_EQUAL(12670,sip_interest(1000,10,1));
+    TEST_ASSERT_EQUAL(1100,lp_interest(1000,10,1));
 }
 
 int main(void)
 {
-    manual_test_add_integers();
+    manual_interest_test();
 
     UnityBegin(NULL);
 
-    RUN_TEST(test_add_integers);
+    RUN_TEST(automated_interest_test);
 
     return (UnityEnd());
 }
